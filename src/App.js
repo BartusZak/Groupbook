4d65c0e9396e79281b997fa4ef9218fd426ead19
polubis <polubis@wp.pl>
2018-03-15 09:22:05 +0100

    Dodanie scroll fixera

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
-import ReallySmoothScroll from 'really-smooth-scroll';
+
+import ScrollFixer from './hoc/ScrollFixer/ScrollFixer';
+
 
 import RootContainer from './containers/RootContainer/RootContainer';
-
-ReallySmoothScroll.shim();
-
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <RootContainer />
-        </div>
+        <ScrollFixer>
+            <div className="App">
+              <RootContainer />
+            </div>
+        </ScrollFixer>
       </Router>
     );
   }
