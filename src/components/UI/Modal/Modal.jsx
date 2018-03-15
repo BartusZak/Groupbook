2476f513beea71772a58d0d31c8115c1c3ca8e36
polubis <polubis@wp.pl>
2018-03-15 12:43:19 +0100

    Dodane bajerow do dodawania postow

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -11,14 +11,14 @@ class Modal extends Component{
     }
 
     render(){
-    
-
+      
         return(
             <Aux>
                 <Backdrop show={this.props.show} clicked={this.props.clickedMethod}/>
                 <div className="Modal"
                     style={{transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: this.props.show ? '1' : '0'}}>
+                    opacity: this.props.show ? '1' : '0', 
+                    top: this.props.heightPosition}}>
                     {this.props.children}
                 </div>
             </Aux>
--- src/containers/UserOptions/AddPost/Addpost.jsx
+++ src/containers/UserOptions/AddPost/Addpost.jsx
@@ -3,21 +3,20 @@ import CenterComponent from '../../CenterComponent/CenterComponent';
 import './Addpost.css';
 import axios from '../../../axios-post';
 import Spinner from '../../../components/UI/Spinner/Spinner';
-
-
-
 import 'font-awesome/css/font-awesome.min.css';
+import Modal from '../../../components/UI/Modal/Modal';
+
 class Addpost extends Component{
     state = {
         groupData: [], 
         showSpinner: false,
         groupLoadingError: false,
+        groupsToPublic: [],
+        numberOfAdded: 0,
+        numberOfGroups: 0,
 
-        groupsToPublic: []
-      
-
+        showModal: false
     }
-
     componentDidMount(){
         this.generatingGroups();
     }
@@ -26,12 +25,11 @@ class Addpost extends Component{
         let oldData = [...this.state.groupData];
         axios.get('/posts/1/comments').then(response => {
             oldData = response.data;
-            this.setState({groupData: oldData, showSpinner: false});
+            this.setState({groupData: oldData, showSpinner: false, numberOfAdded: 0, numberOfGroups: oldData.length});
         }).catch(error => {
             this.setState({groupLoadingError: error, showSpinner: false});
         })
     }
-
     addGroupToPost = (id) => {
         let oldData = [...this.state.groupData];
         const correctValue = oldData.findIndex(p => {
@@ -40,34 +38,50 @@ class Addpost extends Component{
         let oldAdded = [...this.state.groupsToPublic];
         oldAdded.push(oldData[correctValue]);
         oldData.splice(correctValue, 1);
-        this.setState({groupData: oldData, groupsToPublic: oldAdded});
+        this.setState({groupData: oldData, groupsToPublic: oldAdded, numberOfAdded: this.state.numberOfAdded+1});
+    }
+
+    addAllGroups = () => {
+        const concatedArrays = this.state.groupData.concat(this.state.groupsToPublic);
+        this.setState({groupsToPublic: concatedArrays, groupData: [], numberOfAdded: concatedArrays.length});     
+    }
+
+    hideModal = () => {
+        this.setState({showModal: !this.state.showModal});
     }
 
     render(){
-        const GroupItems = ( this.state.groupLoadingError ?
+        console.log(this.state.groupsToPublic);
+        let GroupItems = ( this.state.groupLoadingError ?
         <h3>Wystąpił problem podczas ładowania treści : ( </h3> : this.state.showSpinner ? <Spinner /> :
         this.state.groupData.map(item => {
             return <li key={item.id} onClick={(id) => this.addGroupToPost(item.id)} className="GroupItem">{item.name} <i className="fa fa-plus-circle"></i></li>
         }) );
 
- 
+        if(this.state.numberOfAdded === this.state.numberOfGroups)
+            GroupItems = <h3 style={{color: 'green'}}>Dodałeś już wszystkie grupy, do których należysz</h3>;
+
+        const modalContent = !this.state.numberOfAdded > 0 ? <h2>Nie dodano żadnych grup</h2> : <h3>Dodano następujące grupy: </h3>;
         return (
         <div className="Container">
+             
              <div className="SelectGroupPlace">
                 <h1 style={{marginBottom: '30px'}}>Wybierz grupę docelową</h1>
+                <div className="Buttons">
+                    <i onClick={() => this.addAllGroups()} className="fa fa-paw"></i>
+                    <i className="fa fa-history" onClick={() => this.generatingGroups()}></i>
+                    <i className={this.state.numberOfAdded === 0 ? "Numbers EmptyNumber" : "Numbers"}>+{this.state.numberOfAdded}</i>
+                    
+                    <i onClick={this.hideModal} className="fa fa-columns"></i>
+                </div>
                 <ul className="PlaceForGroupItems">
                     {GroupItems}
                 </ul>
-                <div className="Buttons">
-                    <button className="AddPostButton" style={{position: 'initial'}}>Opublikuj</button>
-                    <i className="fa fa-history" onClick={() => this.generatingGroups()}></i>
-                </div>
-               
+                <button disabled={this.state.numberOfAdded > 0 ? false : true} className="AddPostButton" style={{position: 'initial'}}>Podgląd postu</button>
              </div>
              <div className="Addpost">
                 <h1 style={{marginBottom: '30px'}}>Dodaj zdjęcie i wypełnij pola</h1>
                 <div className="InputsContainer">
-                    
                     <div className="LabelsInputs">
                         <h4>Tytuł postu</h4>
                         <input type="text" placeholder="Dodaj tytuł postu..."/>
@@ -80,8 +94,10 @@ class Addpost extends Component{
                     <input className="AddPhotoInput" type="file" />
                 </div>
             </div>
-          
-            <p>{this.state.lastAdded}</p>
+            <Modal heightPosition='40%' show={this.state.showModal} clickedMethod={this.hideModal}>
+                {modalContent}
+            </Modal>
+      
         </div>
        
         );
