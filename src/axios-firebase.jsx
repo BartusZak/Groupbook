ddee61097803dce3ba5b8d5242b7fbf1a739e7a0
polubis <polubis@wp.pl>
2018-03-17 16:43:52 +0100

    implementacja dodawania postow na serwer dokonczyc jutro

--- src/axios-firebase.jsx
+++ src/axios-firebase.jsx
@@ -1,10 +1,7 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'https://groupbook-91dd2.firebaseio.com'
+    baseURL: 'https://groupbook-6e7d3.firebaseio.com'
 })
 
 export default instance;
-
-
-
 
