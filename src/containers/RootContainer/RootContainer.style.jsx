c8f6a514e3bfbab85a5cd8df8b464c57da6bc360
BartusZak <b.ploszynski@gmail.com>
2018-03-20 21:08:15 +0100

    Dodanie bottom right arrow. (wymaga implementacji)

---
+++ src/containers/RootContainer/RootContainer.style.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const RightMenuExpander = styled.div`
+    position: fixed;
+    bottom: 20px;
+    right: 45px;
+    font-size: 30px;
+    background-color: #343a40;
+    display: flex;
+    border-radius: 50%;
+    width: 32px;
+    height: 32px;
+    justify-content: center;
+    color: white;
+`;
