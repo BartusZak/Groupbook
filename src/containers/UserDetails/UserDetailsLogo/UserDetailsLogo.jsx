3b27c7dfbefc60c7b93d10ba5b05cbc0281a32ab
BartusZak <b.ploszynski@gmail.com>
2018-04-08 21:15:36 +0200

    Obrazy od Artura działają w logged/user/1

--- src/containers/UserDetails/UserDetailsLogo/UserDetailsLogo.jsx
+++ src/containers/UserDetails/UserDetailsLogo/UserDetailsLogo.jsx
@@ -19,10 +19,10 @@ class UserDetailsLogo extends Component {
             loadData () {
                 if ( this.props.id) {
                     if ( !this.state.img250x400) {
-                        axios.get( 'http://groupsconnectsapi.azurewebsites.net/api/users/' + this.props.id + '/avatar')
+                        axios.get( 'http://groupsconnectsapi.azurewebsites.net/api/users/' + this.props.id + '/avatar', {responseType: "blob"})
                             .then( response => {
                                 console.log(response);
-                                this.setState({loading: false, img250x400: response.data});
+                                this.setState({loading: false, img250x400: URL.createObjectURL(response.data)});
                             })
                             .catch(err => {
                                 this.setState({loading: false});
