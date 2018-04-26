55c90541f5e039bcd99fe5e8dd0260c19135acd5
BartusZak <b.ploszynski@gmail.com>
2018-04-26 16:17:30 +0200

    Edit: Posty.

--- src/components/UI/PostsBlock/SinglePostDetails/SinglePostDetails.jsx
+++ src/components/UI/PostsBlock/SinglePostDetails/SinglePostDetails.jsx
@@ -2,25 +2,43 @@ import React from 'react';
 import Aux from 'hoc/Auxi';
 import {apiPicturesUrl} from 'axios/apiPicturesUrl';
 import {Post} from './SinglePostDetails.style';
+import { Col, Container, Row, Footer } from 'mdbreact';
 
 const singlePostDetails = (props) => (
 
     <Post>
-        <p className="post-title-name">{props.post.title}</p>
-        <hr/>
-        <p className="creation-date">{props.post.creationDate.slice(0,10) + " " + props.post.creationDate.slice(11,16)}</p>
-    
-        <p>{props.post.content}</p>
-
-        {(props.post.pictures.length > 0 ?
-            <img 
-                src={apiPicturesUrl+props.post.pictures[0].mediumResolutionPicName} 
-                onError={(e)=>{e.target.src=require('assets/img/404/error-image-generic.png')}} 
-                alt="alt" />
-        :
-            null
-        )}
-
+        <Row>
+            <Col>
+                <p className="post-title-name">{props.post.title}</p>
+                <hr/>
+                <p className="creation-date">{props.post.creationDate.slice(0,10) + " " + props.post.creationDate.slice(11,16)}</p>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <p>{props.post.content}</p>
+                
+                <div className="post-image-container">
+                    {(props.post.pictures.length > 0 ?
+                        <img 
+                            style={{objectFit: "cover"}}
+                            src={apiPicturesUrl+props.post.pictures[0].mediumResolutionPicName} 
+                            onError={(e)=>{e.target.src=require('assets/img/404/error-image-generic.png')}} 
+                            alt="alt" />
+                    :
+                        null
+                    )}
+                </div>
+            </Col>
+            <Col>
+            
+                {props.post.comments.map( (i) => {
+                    return (
+                            <span>{i.content}</span>
+                    )  
+                })}
+            </Col>
+        </Row>
     </Post>
 )
 
