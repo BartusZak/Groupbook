5dc59ae84227e0345dafcd6eab88b4737e652abe
BartusZak <b.ploszynski@gmail.com>
2018-06-02 21:24:42 +0200

    Add: Poprawa styli.

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -23,7 +23,7 @@ const about = () => {
       <Row>
         <Col xs="12" sm="12" className="headerTop">
             <LogoIcon width="100px" />
-            <Logo className="img-fluid"/>
+            <Logo class="img-fluid logo-about-big"/>
             <h1>Ludzie z pasją!</h1>
         </Col>
       </Row>
